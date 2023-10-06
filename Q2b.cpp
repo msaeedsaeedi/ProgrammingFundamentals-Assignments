cc72dd115800bc48f23049317a6e2fca2a081813
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-06 21:14:29 +0500

    Adding Pacal Triangle -+ NOT COMPLETED

--- Q2b.cpp
+++ Q2b.cpp
@@ -22,7 +22,7 @@ using namespace std;
 
 int main()
 {
-    bool OuterDiamond, InnerDiamond, CenterBox;
+    bool OuterDiamond, InnerDiamond, CenterBox, PascalTriangle;
 
     for (int i = 1; i <= 9; i++, cout << endl)
     {
@@ -32,26 +32,42 @@ int main()
 
             InnerDiamond = (i != 1) && ((i + j) == 12 || (j - 6 == i));
 
+            CenterBox = (i >= 8 && j >= 5) && (j <= 13);
+
             if (OuterDiamond)
                 cout << ((i % 2 == 0) ? ("-") : ("*"));
             else if (InnerDiamond)
                 cout << ((i % 2 == 0) ? ("*") : ("-"));
+            else if (CenterBox)
+            {
+                PascalTriangle = (i+j) == 17;
+                if (PascalTriangle)
+                {
+                }
+                else
+                    cout << "*";
+            }
             else
                 cout << " ";
 
-            OuterDiamond = 0, InnerDiamond = 0;
+            OuterDiamond = 0, InnerDiamond = 0, CenterBox = 0, PascalTriangle = 0;
         }
     }
     for (int i = 1; i <= 6; i++, cout << endl)
     {
         for (int j = 1; j <= 17; j++)
         {
+            CenterBox = (j >= 5) && (j <= 13);
+
             if (j == 1 || j == 17)
                 cout << ((i % 2 == 0) ? ("*") : ("-"));
             else if (j == 3 || j == 15)
                 cout << ((i % 2 == 0) ? ("-") : ("*"));
+            else if (CenterBox)
+                cout << "*";
             else
                 cout << " ";
+            CenterBox = 0;
         }
     }
     for (int i = 9; i >= 1; i--, cout << endl)
@@ -62,14 +78,18 @@ int main()
 
             InnerDiamond = (i != 1) && ((i + j) == 12 || (j - 6 == i));
 
+            CenterBox = (i == 9 && j >= 5) && (j <= 13);
+
             if (OuterDiamond)
                 cout << ((i % 2 == 0) ? ("*") : ("-"));
             else if (InnerDiamond)
                 cout << ((i % 2 == 0) ? ("-") : ("*"));
+            else if (CenterBox)
+                cout << "*";
             else
                 cout << " ";
 
-            OuterDiamond = 0, InnerDiamond = 0;
+            OuterDiamond = 0, InnerDiamond = 0, CenterBox = 0;
         }
     }
     return 0;
