a61862c880089ee09f7ac06cd85ed67960fe4c83
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-06 20:56:12 +0500

    B - Added Outer Diamonds

--- Q2b.cpp
+++ Q2b.cpp
@@ -6,3 +6,71 @@
     Section : B
 
 */
+/*
+    Information :
+        Rows    :   12+12 = 24
+        Columns :   8+1+8 = 17
+
+        Center Box Row Start:   8
+        Center Box Row End  :   16
+        Center Box Col Start:   5
+        Center Box Col End  :   13
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool OuterDiamond, InnerDiamond, CenterBox;
+
+    for (int i = 1; i <= 9; i++, cout << endl)
+    {
+        for (int j = 1; j <= 17; j++)
+        {
+            OuterDiamond = (j - 8) == i || (i + j) == 10;
+
+            InnerDiamond = (i != 1) && ((i + j) == 12 || (j - 6 == i));
+
+            if (OuterDiamond)
+                cout << ((i % 2 == 0) ? ("-") : ("*"));
+            else if (InnerDiamond)
+                cout << ((i % 2 == 0) ? ("*") : ("-"));
+            else
+                cout << " ";
+
+            OuterDiamond = 0, InnerDiamond = 0;
+        }
+    }
+    for (int i = 1; i <= 6; i++, cout << endl)
+    {
+        for (int j = 1; j <= 17; j++)
+        {
+            if (j == 1 || j == 17)
+                cout << ((i % 2 == 0) ? ("*") : ("-"));
+            else if (j == 3 || j == 15)
+                cout << ((i % 2 == 0) ? ("-") : ("*"));
+            else
+                cout << " ";
+        }
+    }
+    for (int i = 9; i >= 1; i--, cout << endl)
+    {
+        for (int j = 1; j <= 17; j++)
+        {
+            OuterDiamond = (j - 8) == i || (i + j) == 10;
+
+            InnerDiamond = (i != 1) && ((i + j) == 12 || (j - 6 == i));
+
+            if (OuterDiamond)
+                cout << ((i % 2 == 0) ? ("*") : ("-"));
+            else if (InnerDiamond)
+                cout << ((i % 2 == 0) ? ("-") : ("*"));
+            else
+                cout << " ";
+
+            OuterDiamond = 0, InnerDiamond = 0;
+        }
+    }
+    return 0;
+}
