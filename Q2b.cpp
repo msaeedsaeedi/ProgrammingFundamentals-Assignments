55bf0a08ca1c04fbf0463cd25bce1532235b5dc1
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-06 22:36:17 +0500

    Upper Row Pascal + Finished Upper Row pascal triangle area

--- Q2b.cpp
+++ Q2b.cpp
@@ -22,10 +22,13 @@ using namespace std;
 
 int main()
 {
-    bool OuterDiamond, InnerDiamond, CenterBox, PascalTriangle;
-
+    bool OuterDiamond, InnerDiamond, CenterBox, Not_PascalTriangle, Not_PascalTriangle_RowFlag = false;
+    int Pascal_Coefficent = 1;
+    int NotPascalColumnL = 8;
+    int NotPascalColumnR = 10;
     for (int i = 1; i <= 9; i++, cout << endl)
     {
+        Pascal_Coefficent = 1;
         for (int j = 1; j <= 17; j++)
         {
             OuterDiamond = (j - 8) == i || (i + j) == 10;
@@ -40,21 +43,26 @@ int main()
                 cout << ((i % 2 == 0) ? ("*") : ("-"));
             else if (CenterBox)
             {
-                PascalTriangle = (i+j) == 17;
-                if (PascalTriangle)
-                {
-                }
+                Not_PascalTriangle = (j >= 5 && j <= NotPascalColumnL) || j >= NotPascalColumnR;
+                if (Not_PascalTriangle)
+                    cout << "*", Not_PascalTriangle_RowFlag = true;
                 else
-                    cout << "*";
+                {
+                    cout << Pascal_Coefficent;
+                    // Pascal_Coefficent = Pascal_Coefficent * ((i - 7) - (j - 5)) / (j - 5);
+                }
             }
             else
                 cout << " ";
 
-            OuterDiamond = 0, InnerDiamond = 0, CenterBox = 0, PascalTriangle = 0;
+            OuterDiamond = 0, InnerDiamond = 0, CenterBox = 0, Not_PascalTriangle = 0;
         }
+        if (Not_PascalTriangle_RowFlag)
+            NotPascalColumnL--, NotPascalColumnR++;
     }
     for (int i = 1; i <= 6; i++, cout << endl)
     {
+        Pascal_Coefficent = 1;
         for (int j = 1; j <= 17; j++)
         {
             CenterBox = (j >= 5) && (j <= 13);
@@ -64,11 +72,22 @@ int main()
             else if (j == 3 || j == 15)
                 cout << ((i % 2 == 0) ? ("-") : ("*"));
             else if (CenterBox)
-                cout << "*";
+            {
+                Not_PascalTriangle = (j >= 5 && j <= NotPascalColumnL) || j >= NotPascalColumnR;
+                if (Not_PascalTriangle)
+                    cout << "*", Not_PascalTriangle_RowFlag = true;
+                else
+                {
+                    cout << Pascal_Coefficent;
+                    // Pascal_Coefficent = Pascal_Coefficent * ((i - 7) - (j - 5)) / (j - 5);
+                }
+            }
             else
                 cout << " ";
-            CenterBox = 0;
+            CenterBox = 0, Not_PascalTriangle = 0;
         }
+        if (Not_PascalTriangle_RowFlag)
+            NotPascalColumnL--, NotPascalColumnR++;
     }
     for (int i = 9; i >= 1; i--, cout << endl)
     {
