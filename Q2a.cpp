c5e19b46b3743e75338d7a681f19f1b1336d142d
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-06 15:45:12 +0500

    Draft - 1

--- Q2a.cpp
+++ Q2a.cpp
@@ -6,3 +6,29 @@
     Section : B
 
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    1, 4, 7, 10, 13;
+    int space = 25;
+    for (int i = 1; i <= 15; i++, cout << "*" << endl)
+    {
+        for (int j = 1; j <= space; j++)
+            cout << " ";
+        for (int j = 1; j <= 6; j++)
+            if ((i - 1) % 3 == 0 || j == 1)
+                cout << "*";
+            else
+                cout<<" ";
+        for (int j = 1; j <= 32 - space - 7; j++)
+            cout << " ";
+        if (i % 3 == 0)
+            space -= 5;
+    }
+    for (int i = 1; i <= 32; i++)
+        cout << "*";
+    cout << endl;
+    return 0;
+}
