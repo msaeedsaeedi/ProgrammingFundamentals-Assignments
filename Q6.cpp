4ce77bcdde680c02bff58cbdeaae64912dd36665
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-08 09:52:49 +0500

    Simple Starting Animation #1

--- Q6.cpp
+++ Q6.cpp
@@ -6,3 +6,37 @@
     Section : B
 
 */
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 20; i++)
+    {
+        for (int j = 1; j <= 20; j++, cout << endl)
+        {
+            cout << "\t|";
+
+            for (int k = 1; k <= 21; k++)
+            {
+                if (j & 1)
+                {
+                    if ((k == 2 || k == 20))
+                        cout << "*";
+                    else if (k == 11 && i == j)
+                        cout << "&";
+                    else
+                        cout << " ";
+                }
+                else
+                    cout << " ";
+            }
+            cout << "|";
+        }
+
+        usleep(300000);
+        system("clear");
+    }
+    return 0;
+}
