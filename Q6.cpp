cf186bf2e204307774e8cefe3953f0cddd7c7bcb
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-08 22:19:25 +0500

    Bug Fixed - Indexing + Index starts from 0 + Updated values is displayed at the end + Optimization

--- Q6.cpp
+++ Q6.cpp
@@ -9,13 +9,14 @@
 #include <iostream>
 #include <unistd.h>
 #include <bitset>
+#include <ctime>
 using namespace std;
 
 int main()
 {
     unsigned long long int Tlamps = ~0;
     int lamps, lamps_on = 0;
-
+    int fps_count = 0;
     cout << "Enter Number of Lamps : ";
     cin >> lamps;
 
@@ -23,7 +24,18 @@ int main()
     {
         for (int fps = 1; fps <= lamps * 2; fps++)
         {
+            float t = clock();
             system("clear");
+            lamps_on = 0;
+            for (int i = 0; i < lamps; i++)
+                if ((Tlamps >> i) & 1)
+                    lamps_on++;
+            cout << "\n Round : " << round
+                 << "\n\n Total Lamps : \t" << lamps
+                 << "\t FPS : " << fps_count
+                 << "\n Lamps on : \t" << lamps_on
+                 << "\n Lamps off : \t" << (lamps - lamps_on) << "\n\n"
+                 << endl;
             for (int row = 1; row <= lamps * 2; row++, cout << endl)
             {
                 cout << "\t|";
@@ -32,7 +44,7 @@ int main()
                 {
                     if (row & 1)
                     {
-                        int check = (Tlamps >> (((row - 1) / 2) + 1)) & 1;
+                        int check = (Tlamps >> (((row - 1) / 2))) & 1;
                         if (check && (col == 2 || col == 20))
                             cout << "*";
                         else if (col == 11 && fps == row)
@@ -48,22 +60,18 @@ int main()
                     if (round == 1)
                     {
                         if (((((row - 1) / 2) + 1) % 3) == 0)
-                            Tlamps = Tlamps & ~(1 << (((row - 1) / 2) + 1));
+                            Tlamps = Tlamps & ~(1 << (((row - 1) / 2)));
                     }
                     else if ((((row - 1) / 2) + 1) % round == 0)
                     {
-                        int row_reg = (((row - 1) / 2) + 1);
+                        int row_reg = (((row - 1) / 2));
                         Tlamps = Tlamps ^ (1 << row_reg);
                     }
                 }
                 cout << "|";
             }
-            for (int i = 0; i < lamps; i++)
-                if ((Tlamps >> i) & 1)
-                    lamps_on++;
-            cout << "\n Total Lamps : \t" << lamps << "\n Lamps on : \t" << lamps_on
-                 << "\n Lamps off : \t" << (lamps - lamps_on) << endl;
-            lamps_on = 0;
+            if (fps % 3 == 0 || fps == 1)
+                fps_count = 1 / ((clock() - t) / double(CLOCKS_PER_SEC));
             usleep(300000);
         }
     }
