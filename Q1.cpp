514d0ffcbadd2c4122ec16acf0e628385ecbd532
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-11 22:33:18 +0500

    Gate Splitted

--- Q1.cpp
+++ Q1.cpp
@@ -6,3 +6,23 @@
     Section : B
 
 */
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+
+    unsigned int num = 0;
+    short int Gate_1, Gate_2, Gate_3, Gate_4;
+    Gate_1 = Gate_2 = Gate_3 = Gate_4 = 0;
+    cout << "Numericus Number : ";
+    cin >> num;
+    cout << bitset<32>(num) << endl;
+    Gate_1 = num & 0x3FF;
+    Gate_2 = (num >> 10) & 0x3FF;
+    Gate_3 = (num >> 20) & 0x3FF;
+    Gate_4 = (num >> 30) & 0b11;
+
+    return 0;
+}
