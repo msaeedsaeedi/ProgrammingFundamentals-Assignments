481cddbdc305010dd35da1d4f97eb48d86ae19ae
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-20 16:40:10 +0500

    Optimized code + fixed the PartSize

--- Q6.cpp
+++ Q6.cpp
@@ -19,13 +19,14 @@ int main()
         cin >> lamps;
         cin.ignore(100, '\n');
     } while (lamps <= 0);
-
+    float t = clock();
     int number = lamps;
-    int partSize = 5;
+    int partSize = 64;
     int i = 1;
     while (number > 0)
     {
-        unsigned long long int Tlamps = ~0;
+        unsigned long Tlamps = ~0;
+        const unsigned long ONE = 1;
         int currentPart = min(number, partSize);
         for (int round = 1; round <= (partSize * i) - (partSize - currentPart); round++)
         {
@@ -34,22 +35,23 @@ int main()
                 if (round == 1)
                 {
                     if (row >= 3 && (row % 3) == 0)
-                        Tlamps = Tlamps & ~(1 << (((row - 1) % 5) + 1) - 1);
+                        Tlamps = Tlamps & ~(ONE << (((row - 1) % partSize) + 1) - 1);
                 }
                 else if (row % round == 0)
-                    Tlamps = Tlamps ^ (1 << (((row - 1) % 5) + 1) - 1);
+                    Tlamps = Tlamps ^ (ONE << (((row - 1) % partSize) + 1) - 1);
             }
         }
         for (int j = 0; j < currentPart; j++)
             if ((Tlamps >> j) & 1)
                 lamps_on++;
-
         number -= currentPart;
         i++;
     }
     cout << "\n Total Lamps : \t" << lamps
          << "\n Lamps on : \t" << lamps_on
          << "\n Lamps off : \t" << (lamps - lamps_on)
+         << "\n"
          << endl;
+    cout << "Time : " << clock() - t;
     return 0;
 }
