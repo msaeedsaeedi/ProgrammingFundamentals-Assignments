e841e28a171fbea2276e25c2fb1e0be5eb4799f4
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-22 14:36:14 +0500

    Added Comments

--- Q4.cpp
+++ Q4.cpp
@@ -14,6 +14,10 @@ using namespace std;
 int main()
 {
     int n = -1, x = -1, y = 0;
+
+    /*
+        Input Validation
+    */
     do
     {
         cout << "Enter X [0,x] : ";
@@ -23,6 +27,10 @@ int main()
         cin.ignore(10, '\n');
 
     } while (x < 0);
+
+    /*
+        Input Validation
+    */
     do
     {
         cout << "Enter N [1,n] : ";
@@ -35,13 +43,23 @@ int main()
 
     system("clear");
 
+    /*
+        Check if plot can be drawn or not
+    */
     if (x == 0 && n == 0)
     {
         cout << "Graph can't be displayed due to low points" << endl;
         return 1;
     }
 
+    /*
+        calculate Y value
+    */
     y = pow(x, n) + pow(x, n - 1);
+
+    /*
+        Print Graph
+    */
     cout << "   |    " << endl;
     for (int i = y; i >= 0; i -= 1, cout << endl)
     {
@@ -51,7 +69,14 @@ int main()
 
             for (int j = 0; j <= x; j++)
             {
+                /*
+                    calculate Y value
+                */
                 y = pow(j, n) + pow(j, n - 1);
+
+                /*
+                    Print of related point
+                */
                 if (y == i)
                     cout << "    *";
                 else
