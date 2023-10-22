b307eb6b913318f86348efda21e952768fb02729
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-10-22 14:10:34 +0500

    Added Comments

--- Q1.cpp
+++ Q1.cpp
@@ -11,11 +11,21 @@ using namespace std;
 
 int main()
 {
-
+    /*
+        Variable Decleartion
+        num     : Positive Number
+        Gate_1  : Stores first 10 bits of Positive Number
+        Gate_2  : Stores second 10 bits of Positive Number
+        Gate_3  : Stores third 10 bits of Positive Number
+        Gate_4  : Stores last 2 bits of Positive Number
+    */
     unsigned int num = 0;
     short int Gate_1, Gate_2, Gate_3, Gate_4;
     Gate_1 = Gate_2 = Gate_3 = Gate_4 = 0;
 
+    /*
+        Input Validation
+    */
     int temp = -1;
     do
     {
@@ -26,16 +36,26 @@ int main()
         cin.ignore(1000, '\n');
     } while (temp < 0);
     num = temp;
+
+    /*
+        Printing Binary of Positive Number
+    */
     cout << "\n Binary : ";
     for (int i = 31; i >= 0; i--)
         cout << ((num >> i) & 1);
     cout << endl;
 
+    /*
+        Splitting Positive Number into 4 Gates
+    */
     Gate_1 = num & 0x3FF;
     Gate_2 = (num >> 10) & 0x3FF;
     Gate_3 = (num >> 20) & 0x3FF;
     Gate_4 = (num >> 30) & 0b11;
 
+    /*
+        Printing Gates after splitting
+    */
     cout << " Gate 1 : ";
     for (int i = 9; i >= 0; i--)
         cout << ((Gate_1 >> i) & 1);
@@ -53,10 +73,22 @@ int main()
         cout << ((Gate_4 >> i) & 1);
     cout << endl;
 
+    /*
+        Switching Upon 4th Gate values
+        Logic:
+            As 2 bits have 4 possible combinations
+            00  ->  1st Operation   (Convert Back into number)
+            01  ->  2nd Operation   (Print Hexadecimal)
+            10  ->  3rd Operation   (Swap Gate 1 and Gate 2)
+            11  ->  4th Operation   (Perform Division or Multiplication on two numbers [user])
+    */
     switch (Gate_4)
     {
     case 0:
     {
+        /*
+            Convert Binary Back into Number
+        */
         unsigned int converted = 0;
         converted |= (Gate_1) & 0x3FF;
         converted |= ((Gate_2) & 0x3FF) << 10;
@@ -68,6 +100,9 @@ int main()
     }
     case 1:
     {
+        /*
+            Print Hexadecimal of Number
+        */
         cout << "\n After Conversion " << endl;
         cout << " Hexadecimal : ";
         bool nonZeroFound = false;
@@ -87,6 +122,9 @@ int main()
     }
     case 2:
     {
+        /*
+            Swap Gate 1 and Gate 2
+        */
         Gate_1 ^= Gate_2;
         Gate_2 ^= Gate_1;
         Gate_1 ^= Gate_2;
@@ -104,8 +142,15 @@ int main()
     }
     case 3:
     {
+        /*
+            Perform Multiplication or Division [user] on two numbers [user]
+        */
         cout << "\n";
         char c;
+
+        /*
+            Input Validation
+        */
         do
         {
             cout << " Operation [ Multiply (*) / Divide (/) ] : ";
@@ -114,12 +159,23 @@ int main()
                 cin.clear();
             cin.ignore(1000, '\n');
         } while (c != '*' && c != '/');
+
+        /*
+            Switch Operation
+        */
         switch (c)
         {
         case '*':
         {
+            /*
+                Perform Multiplication with out '*' Operator
+            */
             cout << "\n";
             int a = -1, b = -1, product = 0;
+
+            /*
+                Input Validation
+            */
             do
             {
                 cout << " Enter First Numbers  : ";
@@ -128,6 +184,10 @@ int main()
                     cin.clear(), a = -1;
                 cin.ignore(1000, '\n');
             } while (a < 0);
+
+            /*
+                Input Validation
+            */
             do
             {
                 cout << " Enter Second Numbers : ";
@@ -136,6 +196,10 @@ int main()
                     cin.clear(), b = -1;
                 cin.ignore(1000, '\n');
             } while (b < 0);
+
+            /*
+                Check Sign of resultant
+            */
             bool negative = (a < 0) ^ (b < 0);
             a = abs(a);
             b = abs(b);
@@ -148,8 +212,15 @@ int main()
         }
         case '/':
         {
+            /*
+                Perform Division with out '/' Operator (only integer)
+            */
             int a = -1, b = -1;
             int Quotient = 0;
+            
+            /*
+                Input Validation
+            */
             do
             {
                 cout << " Enter First Numbers  : ";
@@ -158,6 +229,10 @@ int main()
                     cin.clear(), a = -1;
                 cin.ignore(1000, '\n');
             } while (a < 0);
+
+            /*
+                Input Validation
+            */
             do
             {
                 cout << " Enter Second Numbers : ";
@@ -166,6 +241,10 @@ int main()
                     cin.clear(), b = -1;
                 cin.ignore(1000, '\n');
             } while (b <= 0);
+
+            /*
+                Check sign of resultant
+            */
             bool negative = (a < 0) ^ (b < 0);
             a = abs(a), b = abs(b);
             while (a >= b)
